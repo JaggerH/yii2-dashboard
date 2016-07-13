b74dfe27ffae6c67c69acb414e22037079d5d803
Jack.H <jstrive@foxmail.com>
2016-07-13 12:24:08 +0800

    update Sidebar to params

--- views/default/index.php
+++ views/default/index.php
@@ -1,5 +1,4 @@
     <?php
-use common\models\News;
 use jackh\aurora\SideBar;
 ?>
 <div class="dashboard-menu">
@@ -7,107 +6,27 @@ use jackh\aurora\SideBar;
         <h3>Dashboard</h3>
     </div>
 <?=SideBar::widget([
-	"column" => [
-		"页面编辑" => [
-			// "options" => [
-			// 	"data-load" => "#dashboard-content",
-			// 	"data-url" => "site/index",
-			// 	"class" => "selected",
-			// 	"expanded" => "true",
-			// ],
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "edite/index",
-			],
-		],
-		"基金产品" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "fund/index",
-			],
-		],
-		"新闻" => [
-			"submenu" => [
-				"公司新闻" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "news/index?NewsSearch[type]=" . News::NEWS_COMPANY_NEWS,
-				],
-				"友山观点" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "news/index?NewsSearch[type]=" . News::NEWS_COMPANY_VIEWS,
-				],
-				"公司公告" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "news/index?NewsSearch[type]=" . News::NEWS_COMPANY_NOTIFYS,
-				],
-				"友山荐文" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "news/index?NewsSearch[type]=" . News::NEWS_COMPANY_RECOMMAND,
-				],
-				"产品公告" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "news/index?NewsSearch[type]=" . News::NEWS_FUND_NEWS,
-				],
-			],
-		],
-		"基金经理" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "fund-manager/index",
-			],
-		],
-		"公司高管" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "executive/index",
-			],
-		],
-		"分公司信息" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "company/index",
-			],
-		],
-		"预约信息" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "appointment/index",
-			],
-		],
-		"企业荣誉" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "glory/index",
-			],
-		],
-		"资料上传" => [
-			"options" => [
-				"data-load" => "#dashboard-list",
-				"data-url" => "download/index",
-			],
-		],
-		"权限管理" => [
-			"submenu" => [
-				"用户授权" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "admin/assignment",
-				],
-				"角色权限" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "admin/role",
-				],
-				"权限编辑" => [
-					"data-load" => "#dashboard-list",
-					"data-url" => "admin/permission",
-				],
-				"路径权限" => [
-					"data-load" => "#dashboard-content",
-					"data-url" => "admin/route",
-					"expanded" => "true",
-				],
-			],
-		],
-	],
+	/** columns
+	 *	"页面编辑" => [
+	 *		// "options" => [
+	 *		//  "data-load" => "#dashboard-content",
+	 *		//  "data-url" => "site/index",
+	 *		//  "class" => "selected",
+	 *		//  "expanded" => "true",
+	 *		// ],
+	 *		"options" => [
+	 *			"data-load" => "#dashboard-list",
+	 *			"data-url" => "edite/index",
+	 *		],
+	 *	],
+	 *	"基金产品" => [
+	 *		"options" => [
+	 *			"data-load" => "#dashboard-list",
+	 *			"data-url" => "fund/index",
+	 *		],
+	 *	],
+	 **/
+	"column" => Yii::$app->params["dashboardSidebar"],
 	"options" => [
 		"class" => "aurora-sidebar",
 		"id" => "menu-sidebar",
